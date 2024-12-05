4fb459524460280e43f82200281ee4abe9e46969
Vishvam <vishusp77@gmail.com>
2024-12-05 17:47:35 +0530

    update

---
+++ Solution_Day4.java
@@ -0,0 +1,18 @@
+public class Solution_Day4 {
+    static void rotateArr(int arr[], int d) {
+        int n = arr.length;
+        d %= n;
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
