1b9262c2e3addbbe58e14751fb66c78710fe0de3
Vishvam <vishusp77@gmail.com>
2024-12-05 17:39:23 +0530

    update

---
+++ Solution_Day1.java
@@ -0,0 +1,14 @@
+class Solution_Day1 {
+    public int getSecondLargest(int[] arr) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2 && num < max1) {
+                max2 = num;
+            }
+        }
+        return (max2 == Integer.MIN_VALUE) ? -1 : max2;
+    }
+}
