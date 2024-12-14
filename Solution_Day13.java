e82cad806b97a678c8b3f90d964ecb6ecea2d0f9
Vishvam <vishusp77@gmail.com>
2024-12-14 17:24:01 +0530

    update

---
+++ Solution_Day13.java
@@ -0,0 +1,19 @@
+class Solution_Day13 {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            while (arr[i] > 0 && arr[i] <= n && arr[arr[i] - 1] != arr[i]) {
+                int correctIndex = arr[i] - 1;
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
