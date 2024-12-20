ac661efd6a614f8e321f232a1a29b9661978cf69
Vishvam <vishusp77@gmail.com>
2024-12-20 12:58:52 +0530

    update

---
+++ Solution_Day19.java
@@ -0,0 +1,29 @@
+class Solution_Day19 {
+    public static int minChar(String s) {
+        int n = s.length();
+        String combined = s + "#" + new StringBuilder(s).reverse().toString();
+        int[] lps = computeLPSArray(combined);
+        return n - lps[combined.length() - 1];
+    }
+    private static int[] computeLPSArray(String str) {
+        int n = str.length();
+        int[] lps = new int[n];
+        int len = 0;
+        int i = 1;
+        while (i < n) {
+            if (str.charAt(i) == str.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
