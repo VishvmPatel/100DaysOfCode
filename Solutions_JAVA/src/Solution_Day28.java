12f9816e13014615ac422da8c7c5ecfac1eb09d1
Vishvam <vishusp77@gmail.com>
2024-12-29 14:27:06 +0530

    update

---
+++ Solutions_JAVA/src/Solution_Day28.java
@@ -0,0 +1,10 @@
+class Solution_Day28 {
+    int countFreq(int[] arr, int target) {
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target)
+                res++;
+        }
+        return res;
+    }
+}
